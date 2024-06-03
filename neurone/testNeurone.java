238ae389ead5a57c6e4f532a7e443f7e6e84b8e9
Valentin Allmnd <valentin.allmnd@gmail.com>
2024-06-03 15:07:49 +0200

    Add stats to understand

--- neurone/testNeurone.java
+++ neurone/testNeurone.java
@@ -1,14 +1,17 @@
+import java.util.Random;
+
 public class testNeurone
 {
 	public static void main(String[] args)
 	{
 		// Tableau des entrées de la fonction ET (0 = faux, 1 = vrai)
-		final float[][] entrees = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+		//final float[][] entrees = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
 		
 		// Tableau des entrées de la fonction OU (0 = faux, 1 = vrai)
-		//float[][] entreesOU = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+		float[][] entrees= {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
 		// Tableau des sorties de la fonction ET
-		final float[] resultats = {0, 0, 0, 1};
+		//final float[] resultats = {0, 0, 0, 1};
+		final float[] resultats = {0,1,1,1};
 		System.out.println("Initialisation des tableaux");
 		// On crée un neurone taillé pour apprendre la fonction ET
 		final iNeurone n = new NeuroneHeaviside(entrees[0].length);
@@ -16,6 +19,9 @@ public class testNeurone
 		//final iNeurone n = new NeuroneSigmoide(entrees[0].length);
 		//final iNeurone n = new NeuroneReLU(entrees[0].length);
 		
+		// Ajouter du bruit aux entrées
+       
+
 		System.out.println("Apprentissage…");
 		// On lance l'apprentissage de la fonction ET sur ce neurone
 		System.out.println("Nombre de tours : "+n.apprentissage(entrees, resultats));
@@ -44,5 +50,49 @@ public class testNeurone
 			// On affiche cette sortie
 			System.out.println("Entree "+i+" : "+n.sortie());
 		}
+		// Ajouter du bruit aux entrées pour les tests
+        float[][] entreesBruit = ajouterBruitAuxEntrees(entrees, 0.1f); // Ajoute un bruit avec écart-type de 0.1
+		int correct_guess_neurone=0;
+		int repetitions=10;
+        // On affiche chaque cas appris avec bruit
+		for(int nb_ite_bruit=0;nb_ite_bruit<repetitions;++nb_ite_bruit){
+
+		
+        for (int i = 0; i < entreesBruit.length; ++i)
+        {
+            // Pour une entrée donnée bruitée
+            final float[] entree = entreesBruit[i];
+            // On met à jour la sortie du neurone
+            n.metAJour(entree);
+			if (Math.round(n.sortie()) == resultats[i]) {
+				correct_guess_neurone++;
+			}
+            // On affiche cette sortie
+            System.out.print("Entree bruitée " + i + " : [");
+            for (int j = 0; j < entree.length; j++) {
+                System.out.print(entree[j]);
+                if (j < entree.length - 1) System.out.print(", ");
+            }
+            System.out.println("] => Sortie : " + n.sortie());
+        }
+		}
+		float accuracy = (float) correct_guess_neurone / (repetitions * entrees.length);
+        System.out.println("Précision moyenne après " + repetitions + " répétitions : " + accuracy);
 	}
+	 public static float[][] ajouterBruitAuxEntrees(float[][] entrees, float ecartType)
+    {
+        Random random = new Random();
+        float[][] entreesBruit = new float[entrees.length][entrees[0].length];
+
+        for (int i = 0; i < entrees.length; i++)
+        {
+            for (int j = 0; j < entrees[i].length; j++)
+            {
+                // Ajouter un bruit gaussien avec moyenne 0 et écart-type donné
+                entreesBruit[i][j] = entrees[i][j] + (float)random.nextGaussian() * ecartType;
+            }
+        }
+
+        return entreesBruit;
+    }
 }
